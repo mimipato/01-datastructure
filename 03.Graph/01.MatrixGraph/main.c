5083747a8f73541feb2b407199767f7e588a8b9e
Wildwolf <102274352+mimipato@users.noreply.github.com>
2023-02-25 17:20:31 +0800

    更新 main.c
    8改为V8

--- 03.Graph/01.MatrixGraph/main.c
+++ 03.Graph/01.MatrixGraph/main.c
@@ -3,7 +3,7 @@
 
 static void setupMatrixGraph(MGraph *g1) {
 	char *nodeNames[] = {"V1", "V2", "V3", "V4",
-						 "V5", "V6", "V7", "8"};
+						 "V5", "V6", "V7", "V8"};
 	initMGraph(g1, 8, nodeNames, 0, 0);
 	addMGraphEdge(g1, 0, 1, 1);
 	addMGraphEdge(g1, 0, 2, 1);
