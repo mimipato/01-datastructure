97ea8193e09e618b4d02f1286e77582a4daaa2b5
Wildwolf <102274352+mimipato@users.noreply.github.com>
2023-02-27 16:37:31 +0800

    Add files via upload

---
+++ 03.Graph/01.MatrixGraph/01.MatrixGraph/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "matrixGraph.h"
+
+static void setupMatrixGraph(MGraph *g1) {
+	char *nodeNames[] = {"V1", "V2", "V3", "V4",
+						 "V5", "V6", "V7", "V8"};
+	initMGraph(g1, 8, nodeNames, 0, 0);
+	addMGraphEdge(g1, 0, 1, 1);
+	addMGraphEdge(g1, 0, 2, 1);
+	addMGraphEdge(g1, 1, 3, 1);
+	addMGraphEdge(g1, 1, 4, 1);
+	addMGraphEdge(g1, 2, 5, 1);
+	addMGraphEdge(g1, 2, 6, 1);
+	addMGraphEdge(g1, 3, 7, 1);
+	addMGraphEdge(g1, 4, 7, 1);
+	addMGraphEdge(g1, 5, 6, 1);
+}
+
+int main() {
+	MGraph g1;
+	setupMatrixGraph(&g1);
+	printf("have %d num!\n", g1.edgeNum);
+	printf("深度遍历: ");
+	clearMGraphVisit();
+	DFSMGraphTravel(&g1, 0);
+	printf("\n广度遍历: ");
+	clearMGraphVisit();
+	BFSMGraphTravel(&g1, 0);
+	printf("\n");
+	return 0;
+}
